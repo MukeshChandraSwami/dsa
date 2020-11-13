3b7521ac05f3e8b0248f7577a56e4c022381e177
Mukesh Swami <mukesh.swami.501598@gmail.com>
2020-11-13 11:08:05 +0530

    Started practice on array : 13th-Nov-2020

---
+++ DSPractice1/src/com/learn/Main.java
@@ -0,0 +1,9 @@
+package com.learn;
+
+public class Main {
+
+    public static void main(String[] args) {
+         Runnable r = () -> System.out.println("Thread Executed");
+         r.run();
+    }
+}
